9371d444c88119fbc431684c5d0dc19b12d9e980
dokkillo <devgcosta@gmail.com>
2015-09-03 18:33:52 +0200

    modal changes and images

--- js/fjHome.js
+++ js/fjHome.js
@@ -13,4 +13,10 @@ $(document).ready(function(){
 				$(".images").fadeIn("slow");		
 				});							
 			});
+	
+	$(".modal-wide").on("show.bs.modal", function() {
+  var height = $(window).height() - 200;
+  $(this).find(".modal-body").css("max-height", height);
+});
+	
 	});
